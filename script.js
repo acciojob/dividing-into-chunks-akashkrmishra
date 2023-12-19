0552cdcfe62d6e38d75c989da41c4207ea65f4aa
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-19 22:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
+	if(arr.length==0) return [];
   // Write your code here
+	
+	let trackSum=0, ans=[], subarray=[];
+	for(let i=0;i<arr.length;i++){
+		if(trackSum+arr[i]<=n){
+			subarray.push(arr[i]);
+			trackSum+=arr[i];
+		}else{
+			ans.push(subarray);
+			subarray=[arr[i]];
+			trackSum=arr[i];
+		}
+	}
+	ans.push(subarray);
+	return ans;
 };
-
+ 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
